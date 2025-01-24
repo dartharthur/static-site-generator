40c54c38c93353080f7fc342d11a27fc39cbf2b1
Kevin Arthur <kevin.arthur.roy@gmail.com>
2025-01-24 14:40:49 -0800

    Add LeafNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -15,3 +15,14 @@ class HTMLNode():
     
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("Invalid HTML: no value")
+        if not self.tag:
+            return f"{self.value}"
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,8 +1,8 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
-class TextHTMLNode(unittest.TestCase):
+class TestHTMLNode(unittest.TestCase):
     def test_to_html_props(self):
         node = HTMLNode(
             "div",
@@ -48,7 +48,29 @@ class TextHTMLNode(unittest.TestCase):
             node.__repr__(),
             "HTMLNode(p, What a strange world, children: None, {'class': 'primary'})",
         )
-    
-    
+
+class TestLeafNode(unittest.TestCase):
+    def test_value(self):
+        node = LeafNode("p", "This is a paragraph of text.")
+        self.assertEqual(
+            '<p>This is a paragraph of text.</p>', node.to_html()
+        )
+
+    def test_with_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            '<a href="https://www.google.com">Click me!</a>', node.to_html()
+        )
+
+    def test_raises_error(self):
+        with self.assertRaises(ValueError):
+            node = LeafNode("a", None, {"href": "https://www.google.com"})
+            node.to_html()
+
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
+
 if __name__ == "__main__":
     unittest.main()
