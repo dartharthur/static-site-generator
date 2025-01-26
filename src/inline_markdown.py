2aa3ffc71beb2edad3990d4c5ceeee83d4be4f09
Kevin Arthur <kevin.arthur.roy@gmail.com>
2025-01-25 16:26:12 -0800

    Carry over some improvements from solution code

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -41,39 +41,47 @@ def extract_markdown_links(text):
 def split_nodes_image(old_nodes):
     new_nodes = []
     for old_node in old_nodes:
-        split_nodes = []
-        remaining_text = old_node.text
-        links = extract_markdown_images(remaining_text)
-        if not links:
+        if old_node.text_type != TextType.TEXT:
             new_nodes.append(old_node)
             continue
-        for alt_text, image_url in links:
-            first_section_text, second_section_text = remaining_text.split(f"![{alt_text}]({image_url})", 1)
-            if first_section_text:
-                split_nodes.append(TextNode(first_section_text, TextType.TEXT))
-            split_nodes.append(TextNode(alt_text, TextType.IMAGE, image_url))
+        remaining_text = old_node.text
+        images = extract_markdown_images(remaining_text)
+        if len(images) == 0:
+            new_nodes.append(old_node)
+            continue
+        for alt_text, image_url in images:
+            sections = remaining_text.split(f"![{alt_text}]({image_url})", 1)
+            if len(sections) != 2:
+                raise ValueError("Invalid markdown, image section not closed")
+            first_section_text, second_section_text = sections
+            if first_section_text != "":
+                new_nodes.append(TextNode(first_section_text, TextType.TEXT))
+            new_nodes.append(TextNode(alt_text, TextType.IMAGE, image_url))
             remaining_text = second_section_text
-        if remaining_text:
-            split_nodes.append(TextNode(remaining_text, TextType.TEXT))
-        new_nodes.extend(split_nodes)
+        if remaining_text != "":
+            new_nodes.append(TextNode(remaining_text, TextType.TEXT))
     return new_nodes
 
 def split_nodes_link(old_nodes):
     new_nodes = []
     for old_node in old_nodes:
-        split_nodes = []
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
         remaining_text = old_node.text
         links = extract_markdown_links(remaining_text)
-        if not links:
+        if len(links) == 0:
             new_nodes.append(old_node)
             continue
         for link_text, link_url in links:
-            first_section_text, second_section_text = remaining_text.split(f"[{link_text}]({link_url})", 1)
-            if first_section_text:
+            sections = remaining_text.split(f"[{link_text}]({link_url})", 1)
+            if len(sections) != 2:
+                raise ValueError("Invalid markdown, link section not close")
+            first_section_text, second_section_text = sections
+            if first_section_text != "":
                 new_nodes.append(TextNode(first_section_text, TextType.TEXT))
             new_nodes.append(TextNode(link_text, TextType.LINK, link_url))
             remaining_text = second_section_text
-        if remaining_text:
+        if remaining_text != "":
             new_nodes.append(TextNode(remaining_text, TextType.TEXT))
-        new_nodes.extend(split_nodes)
     return new_nodes
